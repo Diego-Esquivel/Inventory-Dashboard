0ba2d13cade0d2bfb39f6db597535cfc36c24c03
Diego-Esquivel <mpi.diegoe@gmail.com>
2025-11-14 11:52:04 -0600

    Create InventoryManagementWebApp.
    Create empty app structure for InventoryManagementWebApp.

---
+++ InventoryDashboard/InventoryManagementWebApp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class InventorymanagementwebappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'InventoryManagementWebApp'
