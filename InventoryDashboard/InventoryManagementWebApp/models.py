35f9fb7be309f2160af239e562fec6ee909b40bb
Diego-Esquivel <mpi.diegoe@gmail.com>
2025-11-14 21:47:05 -0600

    Update Inventory Model to Support Features.
    Updated the Inventory by overloading the create method, adding fields
    for scheduled deletion, and modifying default values. Updated views to
    handle new inventory creation logic. Registered new models in admin for
    management.
    
    Added tests to verify new functionality in inventory
    management. This contributes to asserting the acceptance tests for
    "Add item to inventory with all information", "add item to inventory
    without location", "add item to inventory without quantity", "add item
    to inventory without quantity and location", "delete item from
    inventory as manage", "cannot delete item as non-manage", & "add a
    duplicate label ID".

--- InventoryDashboard/InventoryManagementWebApp/models.py
+++ InventoryDashboard/InventoryManagementWebApp/models.py
@@ -49,7 +49,7 @@ class TransactionHistory(models.Model):
     inventory_item = models.ForeignKey('Inventory', on_delete=models.CASCADE)
     action_name = models.CharField(max_length=4, choices=Actions.choices, default=Actions.CREATED)
     timestamp = models.DateTimeField(auto_now_add=True)
-    performed_by = models.ForeignKey(Associate, on_delete=models.CASCADE)
+    performed_by = models.ForeignKey(Associate, on_delete=models.CASCADE, blank=False, null=False)
     previous_quantity = models.IntegerField(null=True, blank=True)
     new_quantity = models.IntegerField(null=True, blank=True)
     previous_location = models.CharField(max_length=4, null=True, blank=True)
@@ -58,26 +58,75 @@ class TransactionHistory(models.Model):
     def __str__(self):
         return f"TransactionHistory {self.record_id}"
 
+class InventoryManager(models.Manager):
+    def create(self, label_id, product_description, associate, storage_location='HOLD', quantity_on_pallet=-100):
+        # Override create method to create a transaction history record
+        inventory_item = super().create(label_id=label_id, storage_location=storage_location, quantity_on_pallet=quantity_on_pallet,
+                       product_description=product_description
+                       )
+        transaction_history = TransactionHistory.objects.create(
+            inventory_item=inventory_item,
+            action_name=TransactionHistory.Actions.CREATED,
+            performed_by=associate,
+            new_quantity=quantity_on_pallet,
+            new_location=storage_location,
+        )
+        inventory_item.transaction_history = transaction_history
+        inventory_item.save()
+        return inventory_item
+
 class Inventory(models.Model):
     record_id = models.AutoField(primary_key=True)
     label_id = models.CharField(max_length=100)
-    storage_location = models.CharField(max_length=4)
-    quantity_on_pallet = models.IntegerField(default=0)
+    storage_location = models.CharField(max_length=4, default='HOLD')
+    quantity_on_pallet = models.IntegerField(default=-100)
     product_description = models.CharField(max_length=200)
-    transaction_history = models.OneToOneField(TransactionHistory, on_delete=models.CASCADE)
+    transaction_history = models.OneToOneField(TransactionHistory, on_delete=models.CASCADE, blank=True, null=True, default=None)
     scheduled_for_deletion = models.DateTimeField(null=True, blank=True, default=None)
+
+    objects = InventoryManager()
 
     def __str__(self):
         return f"{self.product_name} - {self.quantity} units at location {self.storage_location}"
 
+    def update_quantity(self, new_quantity, associate):
+        # Override update method to create a transaction history record
+        previous_quantity = self.quantity_on_pallet
+        self.quantity_on_pallet = new_quantity
+        self.save()
+        TransactionHistory.objects.create(
+            inventory_item=self,
+            action_name=TransactionHistory.Actions.EDIT_QUANTITY,
+            performed_by=associate,
+            previous_quantity=previous_quantity,
+            new_quantity=new_quantity
+        )
+
+    def update_location(self, new_location, associate):
+        # Override update method to create a transaction history record
+        previous_location = self.storage_location
+        self.storage_location = new_location
+        self.save()
+        TransactionHistory.objects.create(
+            inventory_item=self,
+            action_name=TransactionHistory.Actions.MOVE_LOCATION,
+            performed_by=associate,
+            previous_location=previous_location,
+            new_location=new_location
+        )
+
     def delete(self, associate):
         # Override delete method to create a transaction history record
+        # Ensure the associate is a mangager before allowing deletion
+        if not associate.is_manager:
+            raise PermissionError("Only managers can delete inventory items.")
+        # The associate is a manager; proceed with scheduling for deletion
         self.scheduled_for_deletion = datetime.now()
         self.save()
         # Schedule for deletion after TIME_TO_WAIT_BEFORE_DELETION days
         TransactionHistory.objects.create(
             inventory_item=self,
             action_name=TransactionHistory.Actions.DELETED,
-            performed_by=self.associate  # In a real application, set the associate performing the deletion
+            performed_by=associate  # In a real application, set the associate performing the deletion
         )
         
--- InventoryDashboard/InventoryManagementWebApp/tests.py
+++ InventoryDashboard/InventoryManagementWebApp/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 
 # Create your tests here.
 from django.contrib.auth.models import User
-from .models import Associate
+from .models import *
 
 
 class LoginTests(TestCase):
@@ -35,3 +35,157 @@ class LoginTests(TestCase):
         # Verify that the user is logged out by checking the session
         resp = self.client.get('/select-operations/')
         self.assertEqual(resp.status_code, 302)  # Should redirect to login since user is logged out
+
+class InventoryTests(TestCase):
+    def setUp(self):
+        # Create a test associate
+        self.not_manager = Associate.objects.create(name='testassociate', password='Secr3tPass!', is_manager=False)
+        self.associate = Associate.objects.create(name='inventorymanager', password='Inv3nt0ry!', is_manager=True)
+        # Create a test inventory item
+        self.inventory_item = Inventory.objects.create(
+            label_id='ITEM123',
+            storage_location='A1',
+            quantity_on_pallet=50,
+            product_description='Test Product',
+            associate=self.associate
+        )
+
+    def test_create_inventory_item(self):
+        initial_count = Inventory.objects.count()
+        new_item = Inventory.objects.create(
+            label_id='ITEM456',
+            storage_location='B2',
+            quantity_on_pallet=30,
+            product_description='Another Product',
+            associate=self.associate
+        )
+        new_item = Inventory.objects.get(label_id='ITEM456')
+        self.assertEqual(Inventory.objects.count(), initial_count + 1)
+        self.assertEqual(new_item.label_id, 'ITEM456')
+        self.assertEqual(new_item.storage_location, 'B2')
+        self.assertEqual(new_item.quantity_on_pallet, 30)
+        self.assertEqual(new_item.product_description, 'Another Product')
+    
+    def test_update_inventory_quantity(self):
+        previous_quantity = self.inventory_item.quantity_on_pallet
+        new_quantity = 75
+        self.inventory_item.update_quantity(new_quantity, self.associate)
+        updated_item = Inventory.objects.get(record_id=self.inventory_item.record_id)
+        self.assertEqual(updated_item.quantity_on_pallet, new_quantity)
+        # Verify that a transaction history record was created
+        transaction = TransactionHistory.objects.filter(
+            inventory_item=updated_item,
+            action_name=TransactionHistory.Actions.EDIT_QUANTITY
+        ).first()
+        self.assertIsNotNone(transaction)
+        self.assertEqual(transaction.previous_quantity, previous_quantity)
+
+    def test_update_inventory_location(self):
+        previous_location = self.inventory_item.storage_location
+        new_location = 'C3'
+        self.inventory_item.update_location(new_location, self.associate)
+        updated_item = Inventory.objects.get(record_id=self.inventory_item.record_id)
+        self.assertEqual(updated_item.storage_location, new_location)
+        # Verify that a transaction history record was created
+        transaction = TransactionHistory.objects.filter(
+            inventory_item=updated_item,
+            action_name=TransactionHistory.Actions.MOVE_LOCATION
+        ).first()
+        self.assertIsNotNone(transaction)
+        self.assertEqual(transaction.previous_location, previous_location)
+    
+    def test_can_delete_inventory_item_as_manager(self):
+        self.inventory_item.delete(self.associate)
+        deleted_item = Inventory.objects.get(record_id=self.inventory_item.record_id)
+        self.assertIsNotNone(deleted_item.scheduled_for_deletion)
+        # Verify that a transaction history record was created
+        transaction = TransactionHistory.objects.filter(
+            inventory_item=deleted_item,
+            action_name=TransactionHistory.Actions.DELETED
+        ).first()
+        self.assertIsNotNone(transaction)
+        self.assertEqual(transaction.inventory_item.record_id, deleted_item.record_id)
+        self.assertEqual(transaction.action_name, TransactionHistory.Actions.DELETED)
+        self.assertEqual(transaction.performed_by.id, self.associate.id)
+
+    def test_cannot_delete_inventory_item_as_non_manager(self):
+        with self.assertRaises(PermissionError):
+            self.inventory_item.delete(self.not_manager)
+
+    def test_create_inventory_item_without_location(self):
+        new_item = Inventory.objects.create(
+            label_id='ITEM789',
+            product_description='No Location Product',
+            associate=self.associate,
+            quantity_on_pallet=100
+        )
+        new_item = Inventory.objects.get(label_id='ITEM789')
+        self.assertEqual(new_item.storage_location, 'HOLD')
+        self.assertEqual(new_item.quantity_on_pallet, 100)
+    
+    def test_create_inventory_item_without_quantity(self):
+        new_item = Inventory.objects.create(
+            label_id='ITEM101',
+            product_description='No Quantity Product',
+            associate=self.associate,
+            storage_location='D4'
+        )
+        new_item = Inventory.objects.get(label_id='ITEM101')
+        self.assertEqual(new_item.storage_location, 'D4')
+        self.assertEqual(new_item.quantity_on_pallet, -100)
+    
+    def test_create_inventory_item_without_location_and_quantity(self):
+        new_item = Inventory.objects.create(
+            label_id='ITEM202',
+            product_description='No Location and Quantity Product',
+            associate=self.associate
+        )
+        new_item = Inventory.objects.get(label_id='ITEM202')
+        self.assertEqual(new_item.storage_location, 'HOLD')
+        self.assertEqual(new_item.quantity_on_pallet, -100)
+    
+    def test_cannot_create_inventory_item_without_label_id(self):
+        with self.assertRaises(Exception):
+            Inventory.objects.create(
+                product_description='No Label ID Product',
+                associate=self.associate,
+                storage_location='E5',
+                quantity_on_pallet=20
+            )
+    
+    def test_cannot_create_inventory_item_without_description(self):
+        with self.assertRaises(Exception):
+            Inventory.objects.create(
+                label_id='ITEM303',
+                associate=self.associate,
+                storage_location='F6',
+                quantity_on_pallet=20
+            )
+    
+    def test_cannot_create_inventory_item_without_associate(self):
+        with self.assertRaises(Exception):
+            Inventory.objects.create(
+                label_id='ITEM404',
+                product_description='No Associate Product',
+                storage_location='G7',
+                quantity_on_pallet=20
+            )
+
+    def test_create_inventory_item_with_duplicate_label_id(self):
+        Inventory.objects.create(
+            label_id='DUPLICATE123',
+            product_description='First Product',
+            associate=self.associate,
+            storage_location='H8',
+            quantity_on_pallet=10
+        )
+        # Creating another item with the same label_id should be allowed
+        second_item = Inventory.objects.create(
+            label_id='DUPLICATE123',
+            product_description='Second Product',
+            associate=self.associate,
+            storage_location='I9',
+            quantity_on_pallet=15
+        )
+        self.assertIsNotNone(second_item)
+        self.assertEqual(second_item.label_id, 'DUPLICATE123')
